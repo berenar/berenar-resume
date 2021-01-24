516ea33fbaaf14578b5714cd41c4b80f02d71060
berenar <bernatpericasserra97@gmail.com>
2021-01-24 02:49:02 +0100

    Added favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,70 @@
 import Head from "next/head";
 
 export default function Home() {
-  const a: String = "a"
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Hi! My name is Bernat</title>
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          href="icon-192x192.png"
+        />
+        <link
+          rel="shortcut icon"
+          sizes="192x192"
+          href="icon-192x192.png"
+        />
+        <link rel="apple-touch-icon" href="icon-192x192.png" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <img
+          src="/memoji-portrait.png"
+          alt="Vercel Logo"
+          className="portrait"
+        />
+
+        <h1 className="title">Hi! My name is Bernat</h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Even though some people call me{" "}
+          <code>
+            <a href="https://github.com/berenar">berenar</a>
+          </code>
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="" className="card">
+            <h3>About me &rarr;</h3>
+            <p>Curious and organized</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="" className="card">
+            <h3>Education &rarr;</h3>
+            <p>Life is a lesson</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href=""
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Work &rarr;</h3>
+            <p>3 experiences and couting</p>
+          </a>
+
+          <a
+            href=""
+            className="card"
+          >
+            <h3>Skills &rarr;</h3>
+            <p>Like a Swiss knife</p>
           </a>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+          Bernat Pericàs Serra, 2021
       </footer>
     </div>
   );
